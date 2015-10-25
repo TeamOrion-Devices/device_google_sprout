d04f7b6a9252336d455a0344b15a7939293ba1f2
Akhil Narang <akhilnarang.1999@gmail.com>
2015-10-25 19:45:54 +0100

    sprout: orion bringup
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- orion.mk
+++ orion.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, device/google/sprout/sprout.mk)
 
-# Common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Common orion stuff
+$(call inherit-product, vendor/orion/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=5.1/LMY47O/1783956:user/release-keys PRIVATE_BUILD_DESC="sprout-user 5.1 LMY47O 1783956 release-keys"
 
-PRODUCT_NAME := cm_sprout
+PRODUCT_NAME := orion_sprout
 PRODUCT_DEVICE :=sprout
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
